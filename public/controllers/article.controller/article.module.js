0d65cfd47b8b907255f6b45484fa34af6e42b47c
vittal <vitshacov@gmail.com>
2017-02-01 01:27:16 +0300

    add remove

--- public/controllers/article.controller/article.module.js
+++ public/controllers/article.controller/article.module.js
@@ -1,11 +1,17 @@
 angular.module('articleModule', [])
 	.component('article', {
 	  templateUrl: '/controllers/article.controller/article.template.html',
-	  controller: [ArticleController],
+	  controller: ['$rootScope', '$scope', '$http', ArticleController],
       bindings: {
     	    article: '<' // or key: '<' it depends on what binding you need
       }
 	});
 
 
-function ArticleController() {}
+function ArticleController($rootScope, $scope, $http) {
+	$scope.remove = function (article) {
+		$http.get('/articles/remove/' + article._id)
+			.then(function () {console.log('suc');}, function () {console.log('err');})
+		$rootScope.$broadcast('updateArticles');
+	};
+}
